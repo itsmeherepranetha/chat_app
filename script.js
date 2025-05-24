2a349d1f34b6a9bf930a739608ea4257ef9e454d
itsmeherepranetha <pranethacoding@gmail.com>
2025-05-24 20:26:01 +0530

    basic setup of chat app socket.io

---
+++ script.js
@@ -0,0 +1,37 @@
+// client side
+
+// the io is pulled from the socket.io.js from , included in index.html
+const socket=io('http://localhost:3000') // the place where backend is running
+const messageForm=document.getElementById('send-container');
+const messageContainer=document.getElementById('message-container');
+const messageInput=document.getElementById('message-input');
+//console.log(messageInput);
+
+const appendMessage=message=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageContainer.append(messageElement);
+}
+
+const username=prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user',username);
+
+
+messageForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    socket.emit('send-chat-message',message);
+    appendMessage(`You : ${data.message}`);
+    messageInput.value='';
+})
+
+socket.on('chat-message',data=>{ // recieving the message sent from server.js and then consoling it
+    //console.log(data)
+    appendMessage(`${data.username} : ${data.message}`);
+})
+
+socket.on('user-connected',username=>{ // recieving the message sent from server.js and then consoling it
+    //console.log(data)
+    appendMessage(`${username} has connected`);
+})
---
+++ server.js
@@ -0,0 +1,27 @@
+const {Server}=require('socket.io')
+
+const io=new Server(3000,{
+    cors:{
+        origin:"http://127.0.0.1:5500", // client running here....so connecting backend to the client
+        methods:["GET","POST"],
+    }
+})
+
+const users={};
+
+io.on('connection',socket=>{
+    //console.log("new user");
+    //socket.emit('chat-message-to-console','Hello this is pranetha');
+    socket.on('new-user',username=>{
+        users[socket.id]=username; // socket.id is a unique identiifier for each client connected to the socket
+        socket.broadcast.emit('user-connected',username);
+        console.log(users);
+    })
+    socket.on('send-chat-message',message=>{
+        //console.log(message);
+
+        // send it to everyone on that server , except the person who sent it
+        socket.broadcast.emit('chat-message',{username:users[socket.id],message:message});
+    })
+})
+
