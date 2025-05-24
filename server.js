390549eb5b82c9a5c8924c50067a802d5bad1086
itsmeherepranetha <pranethacoding@gmail.com>
2025-05-25 00:26:48 +0530

    setting up express

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const express=require('express');
+const app=express();
+const http=require('http')
+const httpServer=http.createServer(app);
+
 const {Server}=require('socket.io')
 
-const io=new Server(3000,{
+const io=new Server(httpServer,{ // for websocket requests
     cors:{
         origin:"http://127.0.0.1:5500", // client running here....so connecting backend to the client
         methods:["GET","POST"],
     }
+})
+
+app.set('views','./views');
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+const rooms={};
+
+app.get('/',(req,res)=>{
+    res.render('index',{rooms:rooms});
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomName:req.params.room});
 })
 
 const users={};
@@ -32,3 +52,20 @@ io.on('connection',socket=>{
     })
 })
 
+
+// websocket server ....which here is basically an upgraded version of HTTP(HTTP upgrade through a "handshake")
+httpServer.listen(3000,()=>{ // for normal(express) requests
+  console.log('Server is running on port 3000');
+});
+// httpservers can handle both kind of requests.....
+// normal requests (get and post) are handled by express , and realtime requests are handled by websockets(which are hooked to the http server from which messages are coming)
+// app is just a request handler.....express is built on top of the http server itself , just like websockets
+// app.listen(3000) actually under the hood , creates a http server(http.createServer(app).listen(3000))
+// but here since we are using the websocket server also which is on top of the http server....we cant just write app.listen(3000)
+// we have to create the http server and then attach the express app(http.createServer(app).listen(3000)) , and also attach the websocket by 
+//     const io=new Server(httpServer,{
+//     cors:{
+//         origin:"http://127.0.0.1:5500", // client running here....so connecting backend to the client
+//         methods:["GET","POST"],
+//     }
+// })
