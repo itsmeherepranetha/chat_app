4e10e1754f10c45addb9b0961278e406e331f74f
itsmeherepranetha <pranethacoding@gmail.com>
2025-05-25 21:03:31 +0530

    add room functionality

--- public/script.js
+++ public/script.js
@@ -4,6 +4,8 @@
 const socket=io('http://localhost:3000') // the place where backend is running
 
 const messageContainer=document.getElementById('message-container');
+const messageForm=document.getElementById('send-container');
+const messageInput=document.getElementById('message-input');
 
 const appendMessage=message=>{
     const messageElement=document.createElement('div');
@@ -11,20 +13,22 @@ const appendMessage=message=>{
     messageContainer.append(messageElement);
 }
 
-let username='';
-while(!username || username.trim()==='')username=prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user',username);
+if(messageForm)
+{
+    const username=prompt('What is your name?');
+    //while(!username || username.trim()==='')username=prompt('What is your name?')
+    appendMessage('You joined')
+    socket.emit('new-user',roomName,username);
 
-const messageForm=document.getElementById('send-container');
-const messageInput=document.getElementById('message-input');
-messageForm.addEventListener('submit',e=>{
-    e.preventDefault();
-    const message=messageInput.value;
-    socket.emit('send-chat-message',message);
-    appendMessage(`You : ${message}`);
-    messageInput.value='';
-})
+    messageForm.addEventListener('submit',e=>{
+        e.preventDefault();
+        const message=messageInput.value;
+        socket.emit('send-chat-message',roomName,message);
+        appendMessage(`You : ${message}`);
+        messageInput.value='';
+    })
+}
+//else {console.log("messageform not found")};
 
 socket.on('chat-message',data=>{ // recieving the message sent from server.js and then consoling it
     //console.log(data)
@@ -39,4 +43,17 @@ socket.on('user-connected',username=>{ // recieving the message sent from server
 socket.on('user-disconnected',username=>{ // recieving the message sent from server.js and then consoling it
     //console.log(data)
     appendMessage(`${username} has disconnected`);
+})
+
+
+
+const roomContainer=document.getElementById('room-container');
+socket.on('room-created',room=>{
+    const roomElement=document.createElement('div');
+    roomElement.innerText=room;
+    const roomLink=document.createElement('a');
+    roomLink.href=`/${room}`;
+    roomLink.innerText='join';
+    roomContainer.append(roomElement);
+    roomContainer.append(roomLink);
 })
--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const httpServer=http.createServer(app);
 
 const {Server}=require('socket.io')
 
-const io=new Server(httpServer,{ // for websocket requests
-    cors:{
-        origin:"http://127.0.0.1:5500", // client running here....so connecting backend to the client
-        methods:["GET","POST"],
-    }
-})
+const io=new Server(httpServer) // websocket attached to the httpServer
 
 app.set('views','./views');
 app.set('view engine','ejs');
@@ -23,32 +18,50 @@ app.get('/',(req,res)=>{
     res.render('index',{rooms:rooms});
 })
 
+app.post('/room',(req,res)=>{
+    if(rooms[req.body.room])return res.redirect('/');
+    
+    rooms[req.body.room]={users:{}};
+    res.redirect(`/${req.body.room}`);
+
+    //send message that a new room was created
+    io.emit('room-created',req.body.room)
+})
+
 app.get('/:room',(req,res)=>{
     res.render('room',{roomName:req.params.room});
 })
 
-const users={};
+
+const getUserRooms=(socket)=>{
+    return Object.entries(rooms).reduce((names,[username,room])=>{
+        if(room.users[socket.id])names.push(username);
+        return names;
+    },[])
+}
+
 
 io.on('connection',socket=>{
     //console.log("new user");
     //socket.emit('chat-message-to-console','Hello this is pranetha');
-    socket.on('new-user',username=>{
-        users[socket.id]=username; // socket.id is a unique identiifier for each client connected to the socket
-        socket.broadcast.emit('user-connected',username);
-        console.log(users);
+    socket.on('new-user',(room,username)=>{
+        socket.join(room);
+        rooms[room].users[socket.id]=username; // socket.id is a unique identiifier for each client connected to the socket
+        socket.to(room).emit('user-connected',username);
+        //console.log(users);
     })
 
-    socket.on('send-chat-message',message=>{
-        //console.log(message);
-
+    socket.on('send-chat-message',(room,message)=>{
         // send it to everyone on that server , except the person who sent it
-        socket.broadcast.emit('chat-message',{username:users[socket.id],message:message});
+        socket.to(room).emit('chat-message',{username:rooms[room].users[socket.id],message:message});
     })
 
     //when user disconnects
     socket.on('disconnect',()=>{
-        socket.broadcast.emit('user-disconnected',users[socket.id]);
-        delete users[socket.id];
+        getUserRooms(socket).forEach(room=>{
+            socket.to(room).emit('user-disconnected',rooms[room].users[socket.id]);
+            delete rooms[room].users[socket.id];
+        }) 
     })
 })
 
